53e8c0eb7bba1870fbe060bc36a0e6297693b02d
Zhefan Guo <benguo2003103@gmail.com>
2025-09-03 19:30:21 -0400

    first practice assignment

---
+++ HW/HW1/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../include/MathUtils.h"
+#include "../include/AdvancedMath.h"
+
+using namespace MathUtils;
+using namespace AdvancedMath;
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    int b = 4;
+
+    cout << add(x, y) << endl;
+    cout << multiply(x, y) << endl;
+    cout << square(x) << endl;
+
+    return 0;
+}
